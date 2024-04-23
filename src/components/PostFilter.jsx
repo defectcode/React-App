8c4f4bfa1eecd6d3770e625d83fafa3abb85baf7
Constantin <paladuta05@gmail.com>
2024-04-23 14:28:09 +0300

    add new functions

---
+++ src/components/PostFilter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import MyInput from "./UI/input/MyInput";
+import MySelect from "./UI/select/MySelect";
+
+const PostFilter = ({filter, setFilter}) => {
+
+    return (
+        <div>
+        <MyInput
+            value={filter.query}
+            onChange = {e => setFilter({...filter, query: e.target.value})}
+            placeholder="Search"
+        />
+        <MySelect
+            value={filter.sort}
+            onChange={selectedSort => setFilter({...filter, sort: selectedSort})}
+            defaultValue="Sorted"
+            options={[
+                {value: 'title', name: 'By name'},
+                {value: 'body', name: 'By descriotion'},
+            ]}
+        />
+      </div>
+    )
+} 
+
+export default PostFilter;
